471e2f3f4c5d77e542c0a6be435cc64166dc60a8
Morlan <amorlan@vcd-ch204-06.eastern.ewu.edu>
2017-05-25 14:30:45 -0700

    working with layout

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,15 @@
 // Scripts
+
+var img0 = 'https://unsplash.it/600/300?image=1';
+var img1 = 'https://unsplash.it/600/300?image=3';
+var img1 = 'https://unsplash.it/600/300?image=7';
+
+var maxRand = 3;
+var randNum = Math.floor( Math.random() * maxRand );
+console.log(randNum);
+//Swap Out Banner
+$('header img').attr('src', eval( 'img' + randNum ) );
+
 $('figure').lightGallery({
     selector: 'a',
     mode: 'lg-lollipop'
